00011685d2419a9a1eee89ba692d14ee16c2c926
Michael Roach <michael.roach@xing.com>
2018-10-30 20:10:00 +0000

    Add country picker to airline form

--- lib/myflightmap_web/views/airline_view.ex
+++ lib/myflightmap_web/views/airline_view.ex
@@ -2,4 +2,8 @@ defmodule MyflightmapWeb.AirlineView do
   use MyflightmapWeb, :view
 
   import MyflightmapWeb.FormattingHelpers
+
+  def country_options do
+    Countries.all |> Enum.map(fn c -> {c.name, c.alpha2} end)
+  end
 end
