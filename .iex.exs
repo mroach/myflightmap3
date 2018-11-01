a9d81b564cbab98f881dc944361adaaffb4e4475
mroach <mroach@mroach.com>
2018-11-01 23:00:11 +0100

    nice aliases

---
+++ .iex.exs
@@ -0,0 +1,10 @@
+alias Myflightmap.Repo
+
+alias Myflightmap.Transport
+alias Myflightmap.Transport.{AircraftType, Airline, Airport}
+
+alias Myflightmap.Travel
+alias Myflightmap.Travel.{Flight, Trip}
+
+alias Myflightmap.Accounts
+alias Myflightmap.Accounts.{User}
