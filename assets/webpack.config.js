17466dca33528e1e119d3cca225e2169afa47650
mroach <git@c.mroach.com>
2019-02-21 21:27:34 +0100

    Add webpack. I've succumbed to the great satan.
    It genuinely pains me, but I've installed webpack. As much as I love my ongoing crusade against node and JavaScript in general, there doesn't seem to be a more realistic way to manage assets right now. I miss the days when you had proper binaries for minifying CSS and JS. No more. Make sure you have a couple hundred megs of disk space free to manage node_modules. When things stabilise and I see what I really need for assets I'll probably get rid of this and go back to static files that are manually managed. It's really not that hard.

---
+++ assets/webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const glob = require('glob');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const webpack = require('webpack');
+
+module.exports = (env, options) => ({
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({ cache: true, parallel: true, sourceMap: false }),
+      new OptimizeCSSAssetsPlugin({})
+    ]
+  },
+  entry: {
+      './js/app.js': ['./js/app.js'].concat(glob.sync('./vendor/**/*.js'))
+  },
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, '../priv/static/js')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV !== 'production' ? 'style-loader' : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader'
+      },
+      {
+        test: require.resolve('jquery'),
+        use: [
+          {loader: 'expose-loader', options: 'jQuery'},
+          {loader: 'expose-loader', options: '$'}
+        ]
+      }
+    ]
+  },
+  plugins: [
+    // new ExtractTextPlugin('../css/app.css'),
+    new MiniCssExtractPlugin({ filename: '../css/app.css' }),
+    new CopyWebpackPlugin([{ from: 'static/', to: '../' }]),
+    new webpack.ProvidePlugin({$: 'jquery', jQuery: 'jquery'})
+  ]
+});
