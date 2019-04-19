30c173e629c7e88e10bdabe31cd66f3158b2ba73
mroach <git@c.mroach.com>
2019-04-19 09:43:04 +0200

    Menu burger for mobile

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,4 +16,6 @@ import "phoenix_html"
 // Local files can be imported directly using relative paths, for example:
 // import socket from "./socket"
 
+import "./bulma";
+
 require('../css/theme.scss');
