27232eb047839d7cd4c16d8f7a38411c511bbe45
mroach <git@c.mroach.com>
2019-06-01 09:53:00 +0200

    Don't use memsup, add node and hostname

--- lib/myflightmap_web/controllers/system_controller.ex
+++ lib/myflightmap_web/controllers/system_controller.ex
@@ -10,9 +10,14 @@ defmodule MyflightmapWeb.SystemController do
 
   def stats(conn, _params) do
     {{:input, input}, {:output, output}} = :erlang.statistics(:io)
-    :memsup.start_link
+
+    hostname = case :inet.gethostname do
+      {:ok, hostname} -> hostname |> to_string
+      _ -> nil
+    end
 
     data = %{
+      "elixir" => System.build_info,
       "erlang" => %{
         "schedulers"              => :erlang.system_info(:schedulers),
         "uptime_s"                => :erlang.statistics(:wall_clock) |> elem(0) |> Kernel.div(1000),
@@ -24,15 +29,18 @@ defmodule MyflightmapWeb.SystemController do
         "process_count"           => :erlang.system_info(:process_count),
         "port_count"              => :erlang.system_info(:port_count),
         "ets_count"               => length(:ets.all()),
-        "memory"                  => Map.new(:erlang.memory())
+        "memory"                  => Map.new(:erlang.memory()),
+        "node"                    => :erlang.node |> to_string()
       },
       "system" => %{
         "processors" => :erlang.system_info(:logical_processors_available),
         "os_type"    => :os.type |> Tuple.to_list |> Enum.join("/"),
         "os_version" => :os.version |> Tuple.to_list |> Enum.join("."),
         "arch"       => :erlang.system_info(:system_architecture) |> to_string,
-        "memory"     => Map.new(:memsup.get_system_memory_data),
         "time"       => :os.system_time |> System.convert_time_unit(:nanosecond, :second)
+      },
+      "net" => %{
+        "hostname" => hostname
       }
     }
 
